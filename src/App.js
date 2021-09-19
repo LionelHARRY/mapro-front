1e4ebfc9e29fd80121cee83f33701b0d9dd66340
lionel harry <lharry1@hotmail.fr>
2021-09-19 17:37:35 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+//import Footer from "./components/Footer";
+//import mainbg from './image/main-bg.jpg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-image d-flex align-items-center" id="log-page">
+      <div className="col-3 mx-auto text-center shadow p-3 bg-white rounded" id="signin">
+          <h1>mapro</h1>
+          <p>Signin to access data</p>
+          <input type="email" className="form-control mt-5" placeholder="Email" />
+          <input type="password" className="form-control mt-5" placeholder="Password" />
+          <Button className="mt-5">Sign in</Button>
+          <p className="mt-5">Sign up</p>
+      </div>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import { Button } from 'react-bootstrap';
+const Footer = () => {
+    return (
+        <footer>
+            <Button>Footer</Button>
+        </footer>
+    )
+}
+
+export default Footer
